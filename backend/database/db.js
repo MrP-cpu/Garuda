490184b5eaa6a6e3dfc46a271891f12ec09a8188
Rithk <rithkchaudhary86@gmail.com>
2024-02-09 22:03:09 +0530

    database done

---
+++ backend/database/db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://naruto:jVSNMMGwfSVPzHXV@cluster0.78poq1d.mongodb.net/Garuda");
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    dob: String,
+    mobile: Number
+    
+})
+
+const adminSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    mobile: Number,
+    serviceNo: Number,
+    branchNo: Number
+
+})
+
+const authenticationDataSchema = new mongoose.Schema({
+    aadhar: Number,
+    mobile: Number
+})
+
+const documentSchema= new mongoose.Schema({
+    aadhar: Number,
+    mobile: Number
+})
+
+const User = mongoose.model("user",userSchema);
+const Admin = mongoose.model("admin",adminSchema);
+const AuthData = mongoose.model("authData",authenticationDataSchema);
+const Documents = mongoose.model("documents",documentSchema)
+
+module.exports={
+    User,
+    Admin,
+    AuthData,
+    Documents
+}
